efb35694cd347f22021810354a6feb92cf9b9dd9
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-01 18:29:04 +0530

    Add login fragment

---
+++ app/src/main/java/com/example/shoestoreinventoryapp/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.example.shoestoreinventoryapp
+
+import android.database.DatabaseUtils
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.shoestoreinventoryapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        return binding.root
+    }
+}

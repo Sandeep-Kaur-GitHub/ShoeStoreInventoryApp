028020f2e7d22ccded8b6349ba321115f105a605
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-02 11:43:13 +0530

    Add menu to the ShoeList fragment

--- app/src/main/java/com/example/shoestoreinventoryapp/ShoeListScreenFragment.kt
+++ app/src/main/java/com/example/shoestoreinventoryapp/ShoeListScreenFragment.kt
@@ -1,11 +1,11 @@
 package com.example.shoestoreinventoryapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.shoestoreinventoryapp.R
 import com.example.shoestoreinventoryapp.databinding.FragmentShoeListScreenBinding
 
@@ -14,6 +14,17 @@ class ShoeListScreenFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding=DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_list_screen, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }

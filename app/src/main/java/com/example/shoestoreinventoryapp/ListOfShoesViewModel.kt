3fb78afe2597f1a71cd81f2ff35fe51afa1f387d
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-02 13:04:02 +0530

    Add list of shoes to the ShoeListScreenFragment

--- app/src/main/java/com/example/shoestoreinventoryapp/ListOfShoesViewModel.kt
+++ app/src/main/java/com/example/shoestoreinventoryapp/ListOfShoesViewModel.kt
@@ -1,6 +1,37 @@
 package com.example.shoestoreinventoryapp
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class ListOfShoesViewModel: ViewModel() {
+  /* lateinit var shoeList: MutableList<String>
+   init {
+       getShoeList()
+   }
+    private fun getShoeList()
+    {
+        shoeList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+    }*/
 }
--- app/src/main/java/com/example/shoestoreinventoryapp/ShoeListScreenFragment.kt
+++ app/src/main/java/com/example/shoestoreinventoryapp/ShoeListScreenFragment.kt
@@ -1,20 +1,31 @@
 package com.example.shoestoreinventoryapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.ArrayAdapter
+import androidx.lifecycle.Observer
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.shoestoreinventoryapp.R
 import com.example.shoestoreinventoryapp.databinding.FragmentShoeListScreenBinding
 
 class ShoeListScreenFragment : Fragment() {
+    private lateinit var viewModel: ListOfShoesViewModel
     private lateinit var binding: FragmentShoeListScreenBinding
+    val listItem= listOf<String>("hello", "hi")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding=DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_list_screen, container, false)
+        viewModel=ViewModelProvider(this).get(ListOfShoesViewModel::class.java)
         setHasOptionsMenu(true)
+
+        val arr: ArrayAdapter<kotlin.String>
+        arr= ArrayAdapter(this,android.R.layout.simple_list_item_1,listItem)
+        binding.customListView.adapter=arr
         return binding.root
     }
 
@@ -27,4 +38,6 @@ class ShoeListScreenFragment : Fragment() {
         return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
                 || super.onOptionsItemSelected(item)
     }
+
+
 }
